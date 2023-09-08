89327b364958a5b1305ebfb7912451f18d899e8a
andrijaivkovic <andrija.ivkovic@outlook.com>
2023-09-08 21:07:47 +0200

    added go back to home button to 404 page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,7 @@
-import Main from "../../components/Main/Main";
 import useTitle from "../../hooks/useTitle";
+
+import Main from "../../components/Main/Main";
+import Button from "../../components/Button/Button";
 
 const PageNotFound = () => {
   useTitle("404");
@@ -8,6 +10,11 @@ const PageNotFound = () => {
     <Main className="page-not-found__main">
       <p className="page-not-found__error-code">404</p>
       <p className="page-not-found__body">Page coud not be found</p>
+      <Button
+        linkTo={"/home"}
+        kind="right-arrow"
+        buttonText={"Go to Home page"}
+      />
     </Main>
   );
 };
